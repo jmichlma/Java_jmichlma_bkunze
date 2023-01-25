cf5882c181e07a0782b6dba156069facdea16de9
bkunze420 <bkunze@htl-steyr.ac.at>
2023-01-25 14:27:05 +0100

    implement interface

--- bkunze.java
+++ bkunze.java
@@ -1,7 +1,12 @@
-interface TellYourName{
+
+class bkunze{
 
     public void tell(){
-        System.out.println("bkunze");
-    };
+        System.out.println("bkunze is my name");
+    }
+
+
+
+
 
 }
